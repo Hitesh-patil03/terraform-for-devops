b4b9f3c343a9ebb5d72cb3e52ff8825f1b63c6b3
Hitesh989 <hiteshpatil112004@gmail.com>
2026-07-13 16:10:54 +0530

    remote backend file added

---
+++ remote-backends/remote-backend.tf
@@ -0,0 +1,21 @@
+#s3
+resource "aws_s3_bucket" "remote_s3_bucket" {
+  bucket = "state-lock-remote-bucket"
+
+  tags = {
+    Name        = "state-lock-remote-bucket"
+  }
+}
+
+
+#dynamoDB
+resource "aws_dynamodb_table" "remote_dynamoDB_table" {
+  name           = "state-lock-remote-table"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
