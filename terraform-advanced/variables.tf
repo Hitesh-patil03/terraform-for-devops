9e47dcfb6da371d34b186cebc40838148b763f96
Hitesh989 <hiteshpatil112004@gmail.com>
2026-07-14 12:10:05 +0530

    output.tf added

--- terraform-advanced/variables.tf
+++ terraform-advanced/variables.tf
@@ -18,8 +18,8 @@ variable "ec2_instance_state" {
 
 variable "ec2_instance_volume_size" {
 
-    description = "this variable contains ec2 instance volume size"
-    default = 10
-    type = number
+  description = "this variable contains ec2 instance volume size"
+  default     = 10
+  type        = number
 
 }
