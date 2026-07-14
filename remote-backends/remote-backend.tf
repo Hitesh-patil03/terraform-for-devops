278d6dd5cd1487f0d11609d0811bafd40e2fd526
Hitesh989 <hiteshpatil112004@gmail.com>
2026-07-14 12:11:10 +0530

    remote-backend.tf edited

--- remote-backends/remote-backend.tf
+++ remote-backends/remote-backend.tf
@@ -3,16 +3,16 @@ resource "aws_s3_bucket" "remote_s3_bucket" {
   bucket = "state-lock-remote-bucket"
 
   tags = {
-    Name        = "state-lock-remote-bucket"
+    Name = "state-lock-remote-bucket"
   }
 }
 
 
 #dynamoDB
 resource "aws_dynamodb_table" "remote_dynamoDB_table" {
-  name           = "state-lock-remote-table"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
+  name         = "state-lock-remote-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
 
 
   attribute {
